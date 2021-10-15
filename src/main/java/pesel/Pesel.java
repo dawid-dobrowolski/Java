32311affa6ae4df4bf98712b352cb6ae3501af6c
Dawid <d.dobrowolski626@gmail.com>
2021-10-15 12:13:03 +0200

    Lab1

---
+++ src/main/java/pesel/Pesel.java
@@ -0,0 +1,19 @@
+package pesel;
+
+public class Pesel {
+    private String pesel;
+
+    public Pesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public void compare(Pesel pesel) {
+        int[] wage = new int[] {1,3,7,9,1,3,7,9,1,3,1};
+        int[] position = new int[11];
+       // char just = pesel.charAt();
+     //   for(int i=0;i<wage.length;i++) {
+         //   position[i] = Integer.parseInt(pesel.charAt(i));
+           // sum += (wage[i]);
+       // }
+    }
+}
