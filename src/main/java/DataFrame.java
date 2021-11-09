985b086136db9a765a1f4cb5012d2d921f3e2938
d.dobrowolski626@gmail.com <KontrolaWersji626>
2021-11-09 06:58:11 +0100

    DataFrameV1

--- src/main/java/DataFrame.java
+++ src/main/java/DataFrame.java
@@ -1,5 +1,3 @@
-package javaframe;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
--- src/main/java/javaframe/Main.java
+++
@@ -1,7 +0,0 @@
-package javaframe;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
